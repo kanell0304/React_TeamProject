9f1b97ca127bfdebcb562dbd92bb5a21d715d264
gyeongjun <kanell0304@gmail.com>
2025-08-22 18:23:04 +0900

    add categoryList

--- src/component/ListContext.jsx
+++ src/component/ListContext.jsx
@@ -27,19 +27,46 @@ const ListContext = ({children}) => {
             title : "표본 게시글 제목2",
             content : "표본 게시글 제목2",
             date : now,
-            category : "호러"
+            category : "Horror"
         },
         {
             id : 3,
             title : "표본 게시글 제목3",
             content : "표본 게시글 제목3",
             date : now,
-            category : "로맨스"
+            category : "Romance"
         }
     ]);
 
+    const [categoryList, setCategoryList] = useState([
+        {
+            id : 1,
+            name : "SF"
+        },
+        {
+            id : 2,
+            name : "Romance"
+        },
+        {
+            id : 3,
+            name : "Comedy"
+        },
+        {
+            id : 4,
+            name : "Drama"
+        },
+        {
+            id : 5,
+            name : "Horror"
+        },
+        {
+            id : 6,
+            name : "Action"
+        },
+    ]);
+
     return (
-        <ListProvider.Provider value={{movieList, setMovieList}}>
+        <ListProvider.Provider value={{movieList, setMovieList, categoryList, setCategoryList}}>
             {children}
         </ListProvider.Provider>
     );
