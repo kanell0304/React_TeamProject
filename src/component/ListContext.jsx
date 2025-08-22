fbb64a02543ddcbcd9f55422d490f7cd3856a480
gyeongjun <kanell0304@gmail.com>
2025-08-22 17:38:57 +0900

    setting update

--- src/component/ListContext.jsx
+++ src/component/ListContext.jsx
@@ -9,8 +9,8 @@ const ListContext = ({children}) => {
     const month = today.getMonth() + 1;
     const date = today.getDate();
     const hours = today.getHours().toString().padStart(2, "0");
-    const minutes = today.minutes().toString().padStart(2, "0");
-    const seconds = today.seconds().toString().padStart(2, "0");
+    const minutes = today.getMinutes().toString().padStart(2, "0");
+    const seconds = today.getSeconds().toString().padStart(2, "0");
 
     const now = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`
 
