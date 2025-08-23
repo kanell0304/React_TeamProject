d49fcb5dc4fe81c9d47b51c6601c8006d601b765
hyunjun <yygn934@gmail.com>
2025-08-23 22:52:46 +0900

    delete dummy & connect selectedMovieId

--- src/component/EditList.jsx
+++ src/component/EditList.jsx
@@ -5,10 +5,7 @@ import { useNavigate, useParams } from "react-router-dom";
 
 //상세보기 -> 수정
 const EditList = () => {
-
-        // 파라미터에서 수정하고자 하는 게시글의 id를 추출하는 코드(바로 아래 코드)를 추가해뒀습니다. '//'를 풀고 사용하시면 됩니다. - 이경준
-        // const selectedMovieId = useParams(); // 수정하고자 하는 게시글의 id 추출 ex) selectedMovieId = {id : 1}, selectedMovieId.id = 1
-
+        
         //////////////////////////test/////////////////////////////////////
         const today = new Date();
         const year = today.getFullYear();
@@ -20,53 +17,52 @@ const EditList = () => {
 
         const now = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
         //test용 초기값 더미데이터
-        const movieList1= [
-        {
-            id : 1,
-            title : "표본 게시글 제목1",
-            content : "표본 게시글 제목1",
-            date : now,
-            category : "SF"
-        },
-        {
-            id : 2,
-            title : "표본 게시글 제목2",
-            content : "표본 게시글 제목2",
-            date : now,
-            category : "Horror"
-        },
-        {
-            id : 3,
-            title : "표본 게시글 제목3",
-            content : "표본 게시글 제목3",
-            date : now,
-            category : "Romance"
-        }
-    ];
+    //     const movieList1= [
+    //     {
+    //         id : 1,
+    //         title : "표본 게시글 제목1",
+    //         content : "표본 게시글 제목1",
+    //         date : now,
+    //         category : "SF"
+    //     },
+    //     {
+    //         id : 2,
+    //         title : "표본 게시글 제목2",
+    //         content : "표본 게시글 제목2",
+    //         date : now,
+    //         category : "Horror"
+    //     },
+    //     {
+    //         id : 3,
+    //         title : "표본 게시글 제목3",
+    //         content : "표본 게시글 제목3",
+    //         date : now,
+    //         category : "Romance"
+    //     }
+    // ];
         ////////////////////////////본코드////////////////////////////////////
-        // const {id} = useParams();
+        // 파라미터에서 수정하고자 하는 게시글의 id를 추출하는 코드(바로 아래 코드)를 추가해뒀습니다. '//'를 풀고 사용하시면 됩니다. - 이경준 
+        const selectedMovieId = useParams(); // 수정하고자 하는 게시글의 id 추출 ex) selectedMovieId = {id : 1}, selectedMovieId.id = 1
         const navigate = useNavigate();
 
         //임시사용, provider 전역관리용
-        const [movieList, setMovieList] = useState([]);
+        // const [movieList, setMovieList] = useState([]);
 
         //useEffect storage 전역관리 시 재연결 ∵localstorage초기화 문제로 비활성화
-        // const {movieList, setMovieList} = useContext(ListProvider);
-                
+        const {movieList, setMovieList} = useContext(ListProvider);                
         const [review, setReview] = useState({title:'',content:''});
-        const [currentRv, setCurrentRv]=useState(null);
+        
 
         const onsubmit2 = (e)=>{
             e.preventDefault();
             const editedReview = movieList.map((prev)=>(
-                parseInt(prev.id) === parseInt(currentRv.id) ? {...prev,...review,date:now} : prev ));
+                prev.id === review.id ? {...prev,...review,date:now} : prev )); //currentRv->review
                 setMovieList(editedReview);
                 navigate('/'); // 차후 상세페이지로 연결
                 
                 console.log("수정리뷰리스트",editedReview);
             
         }
-
 
 
         ///provider 전역관리?
@@ -78,23 +74,13 @@ const EditList = () => {
         useEffect(()=>{
             const savedList = JSON.parse(localStorage.getItem("reviews")) || [];
             setMovieList(savedList);
-            //상세페이지 작성 후 연결 : useParam()  :id
-            // const currentRv = savedList.find((getRv)=>parseInt(getRv.id)===parseInt(id))            
-            // if(currentRv) setReview(currentRv);
+            
+            //상세페이지 작성 후 연결 : useParam()  :selectedMovieid.id 로변경
+            const currentRv = savedList.find((getRv)=>parseInt(getRv.id)===parseInt(selectedMovieId.id))            
+            if(currentRv) setReview(currentRv);
             console.log("렌더링리스트:",savedList);
-        }, []);
-
-
-        
-        //test///////////////////////////////////
-        if(currentRv)
-        console.log("선택된리뷰:",currentRv.id);
-        else console.log("선택된리뷰없음");        
-        const testselect = () => { //수정버튼 누르면 넘어와서
-            setCurrentRv(movieList[0]);
-            setReview({title:movieList[0].title, content:movieList[0].content})
-        }
-        ///////////////////////////////////
+            console.log('currentRv:',currentRv);
+        }, []);       
         
 
     return (
@@ -105,34 +91,7 @@ const EditList = () => {
                 <textarea value={review.content} onChange={e=>setReview({...review,content:e.target.value})}/><br/>           
                 <button type="submit">수정</button>
             </form>
-            {/* test */}
-            <hr />
-            <h3>테스트용 리스트</h3>
-            <ul>
-                {movieList.map(rv=> (                   
-                        
-                        <li key={rv.id}>
-                        {rv.date}
-                        {rv.title}
-                        {rv.content}
-                        </li>                                           
-                    )
-                )}
-                
-            </ul>
-            {/* selected ID */}
-            <button onClick={testselect}>첫번째 임시선택</button>   
-
-            <br/>
-              <hr></hr>
-            {/* end test */}
-            <button onClick={() => {
-            localStorage.setItem("reviews", JSON.stringify(movieList1));
-            alert("movieList1");
-            }}>
-            강제 초기화
-            </button>
-
+            
         </div>
     );
 };
