be2441b34a63b0724f6cdd9f6b0eda8055b2d669
hyunjun <yygn934@gmail.com>
2025-08-23 23:48:12 +0900

    connect edit-button to EditList.jsx

--- src/component/EditList.jsx
+++ src/component/EditList.jsx
@@ -6,7 +6,7 @@ import { useNavigate, useParams } from "react-router-dom";
 //상세보기 -> 수정
 const EditList = () => {
         
-        //////////////////////////test/////////////////////////////////////
+        //use
         const today = new Date();
         const year = today.getFullYear();
         const month = today.getMonth() + 1;
@@ -16,73 +16,37 @@ const EditList = () => {
         const seconds = today.getSeconds().toString().padStart(2, "0");
 
         const now = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
-        //test용 초기값 더미데이터
-    //     const movieList1= [
-    //     {
-    //         id : 1,
-    //         title : "표본 게시글 제목1",
-    //         content : "표본 게시글 제목1",
-    //         date : now,
-    //         category : "SF"
-    //     },
-    //     {
-    //         id : 2,
-    //         title : "표본 게시글 제목2",
-    //         content : "표본 게시글 제목2",
-    //         date : now,
-    //         category : "Horror"
-    //     },
-    //     {
-    //         id : 3,
-    //         title : "표본 게시글 제목3",
-    //         content : "표본 게시글 제목3",
-    //         date : now,
-    //         category : "Romance"
-    //     }
-    // ];
-        ////////////////////////////본코드////////////////////////////////////
-        // 파라미터에서 수정하고자 하는 게시글의 id를 추출하는 코드(바로 아래 코드)를 추가해뒀습니다. '//'를 풀고 사용하시면 됩니다. - 이경준 
+        //
+        
+        // 파라미터에서 수정하고자 하는 게시글의 id를 추출하는 코드(바로 아래 코드)를 추가해뒀습니다. '//'를 풀고 사용하시면 됩니다. - 이경준 -감사합니다
         const selectedMovieId = useParams(); // 수정하고자 하는 게시글의 id 추출 ex) selectedMovieId = {id : 1}, selectedMovieId.id = 1
         const navigate = useNavigate();
-
-        //임시사용, provider 전역관리용
-        // const [movieList, setMovieList] = useState([]);
-
-        //useEffect storage 전역관리 시 재연결 ∵localstorage초기화 문제로 비활성화
-        const {movieList, setMovieList} = useContext(ListProvider);                
+        
+        const {movieList, setMovieList} = useContext(ListProvider);
         const [review, setReview] = useState({title:'',content:''});
         
-
         const onsubmit2 = (e)=>{
             e.preventDefault();
             const editedReview = movieList.map((prev)=>(
-                prev.id === review.id ? {...prev,...review,date:now} : prev )); //currentRv->review
+                prev.id === review.id ? {...prev, ...review, date:now} : prev ));
                 setMovieList(editedReview);
-                navigate('/'); // 차후 상세페이지로 연결
-                
-                console.log("수정리뷰리스트",editedReview);
-            
+                navigate('/');                      
         }
 
-
-        ///provider 전역관리?
+        //localStorage Context 전역관리용 
         useEffect(()=>{
-            if(movieList.length>0) // //임시사용, provider 전역관리용
-            localStorage.setItem("reviews",JSON.stringify(movieList));
+            if(movieList.length>0) 
+            localStorage.setItem("lists",JSON.stringify(movieList));
         },[movieList]);
         
         useEffect(()=>{
-            const savedList = JSON.parse(localStorage.getItem("reviews")) || [];
-            setMovieList(savedList);
-            
-            //상세페이지 작성 후 연결 : useParam()  :selectedMovieid.id 로변경
-            const currentRv = savedList.find((getRv)=>parseInt(getRv.id)===parseInt(selectedMovieId.id))            
+            const savedList = JSON.parse(localStorage.getItem("lists")) || [];
+            setMovieList(savedList);     
+            //수정폼에 기존내용 입력
+            const currentRv = savedList.find((getRv)=>parseInt(getRv.id) === parseInt(selectedMovieId.id));
             if(currentRv) setReview(currentRv);
-            console.log("렌더링리스트:",savedList);
-            console.log('currentRv:',currentRv);
         }, []);       
         
-
     return (
         <div>
             <h2>리뷰글 수정</h2>
@@ -90,8 +54,7 @@ const EditList = () => {
                 <input value={review.title} onChange={e=>setReview({...review,title:e.target.value})} type="text" /><br/>
                 <textarea value={review.content} onChange={e=>setReview({...review,content:e.target.value})}/><br/>           
                 <button type="submit">수정</button>
-            </form>
-            
+            </form>            
         </div>
     );
 };
