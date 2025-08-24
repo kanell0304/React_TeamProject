afbc8d928913507980674abaa056025b5cd09d6c
hyunjun <yygn934@gmail.com>
2025-08-24 12:27:45 +0900

    add getNow() for update time in Edit.jsx

--- src/customHook/useHooks.jsx
+++ src/customHook/useHooks.jsx
@@ -5,3 +5,17 @@ const useHooks = () => {
 };
 
 export default useHooks;
+
+//EditList 날짜,시간 갱신용 헬퍼함수 
+export const getNow = ()=>{
+    const today = new Date();
+        const year = today.getFullYear();
+        const month = today.getMonth() + 1;
+        const date = today.getDate();
+        const hours = today.getHours().toString().padStart(2, "0");
+        const minutes = today.getMinutes().toString().padStart(2, "0");
+        const seconds = today.getSeconds().toString().padStart(2, "0");
+        
+        const now = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
+        return now;
+}
