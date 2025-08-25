7952f03dddb6f52df01f5951cc514f977d385a90
gyeongjun <kanell0304@gmail.com>
2025-08-25 17:49:50 +0900

    update ListContext

--- src/component/ListContext.jsx
+++ src/component/ListContext.jsx
@@ -1,18 +1,11 @@
 import { useState, createContext } from "react";
+import { getNow } from "../customHook/useHooks";
 
 export const ListProvider = createContext();
 
 const ListContext = ({children}) => {
 
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = today.getMonth() + 1;
-    const date = today.getDate();
-    const hours = today.getHours().toString().padStart(2, "0");
-    const minutes = today.getMinutes().toString().padStart(2, "0");
-    const seconds = today.getSeconds().toString().padStart(2, "0");
-
-    const now = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`
+    const now = getNow();
 
     const [movieList, setMovieList] = useState([
         {
