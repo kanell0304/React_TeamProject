ffa50c9a0464b319fb540488c8b9a8c8173de086
gyeongjun <kanell0304@gmail.com>
2025-08-25 13:18:41 +0900

    remove tempCss/List.css

--- src/component/List.jsx
+++ src/component/List.jsx
@@ -1,6 +1,5 @@
 import { useContext, useEffect, useState } from "react";
 import { ListProvider } from "./ListContext";
-import "../tempCss/List.css"
 import { useNavigate } from "react-router-dom";
 
 const List = () => {
@@ -26,11 +25,11 @@ const List = () => {
     return (
         <div style={{width : "50%", margin : "0px auto"}}>
             <button onClick={createTempList}>임시 리스트 생성 및 초기화</button>
-            <div className="movieList" style={{backgroundColor : "#dee2e6"}}><div style={{width : "20%"}}>번호</div><div style={{width : "50%"}}>내용</div><div style={{width : "30%"}}>작성일자</div></div>
+            <div style={{width : "100%", minWidth : "800px", padding : "10px", display : "flex", border : "1px solid black", backgroundColor : "#dee2e6"}}><div style={{width : "20%", textAlign : "center"}}>번호</div><div style={{width : "50%", textAlign : "center"}}>내용</div><div style={{width : "30%", textAlign : "center"}}>작성일자</div></div>
             {movieList1 && movieList1.map(movie => {
                 return (
-                    <div key={movie.id} className="movieList" onClick={() => moveToDetail(movie.id)}>
-                        <div style={{width : "20%"}}>{movie.id}</div><div style={{width : "50%"}}>{movie.content}</div><div style={{width : "30%"}}>{movie.date}</div>
+                    <div key={movie.id} onClick={() => moveToDetail(movie.id)} style={{minWidth : "800px", width : "100%", padding : "10px", display : "flex", border : "1px solid black"}}>
+                        <div style={{width : "20%", textAlign : "center"}}>{movie.id}</div><div style={{width : "50%", textAlign : "center"}}>{movie.content}</div><div style={{width : "30%", textAlign : "center"}}>{movie.date}</div>
                     </div>
                 )
             })}
