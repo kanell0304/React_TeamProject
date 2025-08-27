f7375e3cc1cf243a717aaf80c25fd0c61f8efa6e
gyeongjun <kanell0304@gmail.com>
2025-08-27 18:12:04 +0900

    editcss List.jsx, ListDetail.jsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-            colors: {
+      colors: {
         'main_gray': 'rgb(172, 187, 207)',
+        'main_gray_a' : 'rgba(172, 187, 207, 0.4)',
+        'hover_main_gray' : 'rgb(152,167,187)'
       }
     },
   },
